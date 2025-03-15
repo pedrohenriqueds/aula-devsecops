6b982f84170e2546fc10babc60e37179cfe4b2e0
Pedro Henrique <106681547+pedrohenriqueds@users.noreply.github.com>
2025-03-15 03:15:33 -0300

    Create soma.py

---
+++ soma.py
@@ -0,0 +1,19 @@
+import argparse
+
+if __name__ == '__main__':
+
+  parser = argparse.ArgumentParser(
+        usage='%(prog)s --a a--b b',
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+  parser.add_argument("-a", "--a",type=str, required=True)
+  parser.add_argument("-b", "--b",type=str, required=True)
+
+  args = parser.parse_args()
+
+  a=args.a
+  b=args.b
+
+  soma=int(a)+int(b)
+
+  print(soma)
